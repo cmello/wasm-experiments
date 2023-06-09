930ed557b971bb54ebdd5c8a83bc51b556d13dcd
Cesar Mello <cmello@gmail.com>
2023-06-08 21:41:52 -0700

    Accessing memory exported by wasm

--- sum_sample/test.cpp
+++ sum_sample/test.cpp
@@ -1,4 +1,9 @@
+__attribute__((import_module("my_module"), import_name("my_function")))
+extern "C" void my_func(const char* message);
+
+
 extern "C" int sum(int op1, int op2)
 {
+  my_func("abcd");
   return op1 + op2;
 }
